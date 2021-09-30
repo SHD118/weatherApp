db86a857ec6153b242b61533bcdd2ca4256df045
Hiro Protagonist <shdesai118@gmail.com>
2021-09-30 17:03:39 -0600

    testing

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var inputText = document.getElementById("inputCity")
 var button = document.getElementById("button-addon2");
 var fiveDay = document.getElementById("5day")
+var weatherDetail = document.getElementById("weather_detail")
 var finalValue = [];
 button.addEventListener("click", function () {
     var inputGlobal = inputText.value;
@@ -31,23 +32,27 @@ function dataInput(input) {
             let weatherArray = []
             for (let i = 0; i < data.list.length; i++) {
                 // console.log(data.list[i].main.temp_min)
+               
 
                 var dt = data.list[i].dt_txt
                 var temp = data.list[i].main.temp
                 var humid = data.list[i].main.humidity
-                 var icon = data.list[i].weather.icon
+                var icon = data.list[i].weather[0].icon
+                console.log("icon " + icon)
+                let tempString = `http://openweathermap.org/img/wn/${icon}.png`
                 var wind = data.list[i].wind.speed;
                 let creatElemtn = $(`<div class="col-2">
                 <div class="card" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">${dt}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">${temp}</h6>
-                    <p class="card-text">${icon}</p>
+                    <p class="card-text"><img src = "${tempString}"/></p>
                     <p class="card-text">${humid}</p>
                     <p class="card-text">${wind}</p>
                 </div>
                </div>
                </div>`)
+               
  creatElemtn.appendTo(fiveDay)
 
             }
@@ -78,19 +83,25 @@ function dataInput2(input) {
 
         })
         .then(function (data) {
-            for (let i = 0; i < data.length; i++){
-                var pTagName = document.createElement("p")
+            // for (let i = 0; i < data.length; i++){
+                var pTagName = document.createElement("h1")
                 var pTagTemp = document.createElement("p")
                 var pTagWind = document.createElement("p")
                 var pTagHumidity = document.createElement("p")
                 var pTagUV = document.createElement("p")
                 pTagName.textContent = data.name;
-                // pTagTemp.textContent = data.main.temp;
-                // pTagWind.textContent = data.wind.speed;
-                // pTagHumidity.textContent = data.main.humidity;
-                // pTagUV = data.uvi
-                pTagName.appendTo("#weather_detail")
-            }
+                pTagTemp.textContent = data.main.temp;
+                pTagWind.textContent = data.wind.speed;
+                pTagHumidity.textContent = data.main.humidity;
+                pTagUV.textContent = data.uvi
+                // pTagName.append(weatherDetail)
+            weatherDetail.appendChild(pTagName)
+            weatherDetail.appendChild(pTagTemp)
+            weatherDetail.appendChild(pTagWind)
+            weatherDetail.appendChild(pTagHumidity)
+            weatherDetail.appendChild(pTagUV)
+            // }
+            return;
         })
 
 }

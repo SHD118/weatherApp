bcd11c41bc1efa7f10c7a01ceab74d03a3481746
Hiro Protagonist <shdesai118@gmail.com>
2021-09-30 09:19:28 -0600

    added https

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var inputText = document.getElementById("inputCity")
 var button = document.getElementById("button-addon2");
-
+var finalValue = [];
 button.addEventListener("click", function () {
     var inputGlobal = inputText.value;
     dataInput(inputGlobal);
@@ -10,14 +10,25 @@ button.addEventListener("click", function () {
 
 function dataInput(input) {
     const myKey = "8d16f28b545852d623de7ad3baf04f51";
+    
     console.log(input)
     console.log(myKey)
-    fetch("api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + myKey)
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&cnt=15" + "&appid=" + myKey)
+        // fetch("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8d16f28b545852d623de7ad3baf04f51")
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data)
+            finalValue = data;
+            // console.log(finalValue)
+            return data;
+
         })
-    return data;
+   
 }
+
+// createweekForCast(){
+
+// }
+

251b6b5af77a55d00b26866476ec175350c43163
Hiro Protagonist <shdesai118@gmail.com>
2021-09-30 10:55:04 -0600

    created forloop

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 var inputText = document.getElementById("inputCity")
 var button = document.getElementById("button-addon2");
+var fiveDay = document.getElementById("5day")
 var finalValue = [];
 button.addEventListener("click", function () {
     var inputGlobal = inputText.value;
     dataInput(inputGlobal);
     // console.log(inputGlobal)
-
+   
 })
 
 function dataInput(input) {
     const myKey = "8d16f28b545852d623de7ad3baf04f51";
-
+    
     console.log(input)
     console.log(myKey)
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + input +  "&appid=" + myKey)
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&cnt=5" + "&appid=" + myKey)
         // fetch("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8d16f28b545852d623de7ad3baf04f51")
-        //  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&cnt=15" + "&appid=" + myKey)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            // console.log(data)
-            // finalValue = data;
+            console.log(data)
+            finalValue = data;
             // console.log(finalValue)
             return data;
 
         })
-        .then(data => {
-            for (let i = 0; i < 5; i++) {
-                Document.getElementById("weather_detail" + (i + 1) + "Min" + Number(data.list[i].main.temp_min ) + "deg")
+        .then(function (data) {
+            let weatherArray = []
+            for (let i = 0; i < data.list.length; i++){
+                // console.log(data.list[i].main.temp_min)
+               
+                var dt = data.list[i].dt_txt
+                var temp = data.list[i].main.temp
+                var humid = data.list[i].main.humidity
+                // var icon = data.list[i].weather.icon
+                var wind = data.list[i].wind.speed;
+           
             }
-})
+            
+        })
+ 
 
+   
 }
+
 
 // createweekForCast(){
 
